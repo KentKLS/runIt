d4d88f01c57ee1f625a73ab9814a5cf9f38e31e6
martin.vasseur <martin.vasseur@le-campus-numerique.fr>
2023-07-05 13:26:21 +0200

    Footer terminé

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="{{asset('styles.css')}}" type="text/css">
+    <link href="https://fonts.googleapis.com/css2? display=swap » rel=" stylesheet>
+
+</head>
+
+<footer class="footer">
+    <div class="container">
+        <div class="columns">
+            <div class="columns__column logo_position">
+                <img class="logo" src='/logo/Calque_1-2.svg' alt="logo">
+            </div>
+            <div class="columns__column columns_position">
+                <p>Politique de confidentialité</p>
+                <p>Condition de vente</p>
+                <p>Nous contacter</p>
+            </div>
+            <div class="columns__column columns_position">
+                <p>Catalogue</p>
+                <p>Trail</p>
+                <p>Running</p>
+            </div>
+            <div class="columns__column columns_follow">
+                <p>Nous suivre:</p>
+                <div class="follow">
+                    <img class="img_footer" src="/img/facebook.png" alt="logo facebook">
+                    <img class="img_footer" src="/img/instagram.png" alt="logo instagram">
+                    <img class="img_footer" src="/img/strava.svg" alt="logo strava">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
