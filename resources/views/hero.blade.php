f9a4e917fe8e975afeba36372c626be72d309cd1
Quentin SIRJEAN <quentin.sirjean@Digital-Grenoble.local>
2023-07-05 14:54:45 +0200

    ajout d'un élément hero

---
+++ resources/views/hero.blade.php
@@ -0,0 +1,12 @@
+
+ <div class=" relative ">
+    <img src="{{URL::asset('Image/HeroHomepage.png')}}" alt="" class="relative">
+    <div class="  max-w-[1400px] mx-auto absolute left-1/4 top-10 object-fill ">
+        <img src="{{ URL::asset('Image/LogoRunIt.svg') }}" alt="logo" class=" absolute top-28 w-3/4 opacity-25">
+        <div class="  text-white z-50 mt-4 sm:mt-20 md:mt-60   ">
+            <h1 class=" text-5xl ">SO CLOSE TO THE</h1>
+            <h1 class=" text-5xl text-[#D2F306] ">FINISH LINE</h1>
+            <p class="p-3 text-2xl">Accéssoires de running pour la performance</p>
+        </div>
+    </div>
+ </div>
