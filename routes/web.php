ec24de53faa3b65f4a3cc8371eafae7997d6a304
Quentin <quentin.sirjean@le-campus-numerique.fr>
2023-07-05 17:09:27 +0200

    added new routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'show'])->name('showHome');
+Route::get('/cart', [CartController::class, 'show'])->name('showCart');
+
