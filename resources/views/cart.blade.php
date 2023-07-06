5b2bd38bec9218a1c5ebdbf0801408bc1f171f65
Quentin <quentin.sirjean@le-campus-numerique.fr>
2023-07-06 11:36:57 +0200

    changement hero + cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -3,7 +3,7 @@
     Cart
 @endsection
 @section('content')
-    <div class="container grid grid-cols-6 grid-rows-6 gap-8 pt-36 pb-10  ">
+    <div class="container grid grid-cols-6 grid-rows-6 gap-8 py-36  montserrat ">
         <div class=" bg-[#D2F306] font-bold row-span-1 col-span-4 pl-12 p-2">
             Panier
         </div>
@@ -11,31 +11,66 @@
             Récapitulatif
         </div>
 
-        <div class='row-span-4 grid grid-cols-5 col-span-4 outline outline-neutral-400'>
-            <div class="col-span-2 flex flex-col">
+        <div class='row-span-4 grid grid-cols-5 grid-rows-4 col-span-4 outline outline-neutral-400 '>
+
+            <div class="col-span-2 row-span-1 flex items-center justify-center ">
                 <p>Produit</p>
-                <img src="" alt="">
             </div>
-            <div class="col-span-1 flex flex-col">
+
+
+            <div class="col-span-1 flex items-center justify-center">
                 <p>Prix unitaire</p>
-                <p>118€</p>
+
             </div>
-            <div class="col-span-1 flex flex-col">
+            <div class="col-span-1  flex items-center justify-center ">
                 <p>Quantité</p>
-                <form action=""><input type="number"> <input></form>
+
+
 
             </div>
-            <div class="col-span-1 flex flex-col">
+            <div class="col-span-1 flex items-center justify-center">
                 <p>Prix total</p>
+
+            </div>
+            <div class="col-span-2 row-span-3 flex flex-col items-center justify-center">
+                <div class="font-bold">Nom du Produit</div>
+                <img src="{{ URL::asset('Image/crosscall-x-chest-electronique-269219-1-f.jpg') }}" alt="" class=" w-3/4 ">
+            </div>
+            <div class="col-span-1 row-span-1 flex items-center justify-center">
+                <p class=" flex items-center justify-center ">118€</p>
+            </div>
+            <div class="col-span-1 row-span-1 flex items-center justify-center">
+                <form action="" >
+                    <input type="button" onclick="decrementValue()" class="px-1 font-extrabold text-xl text-[#FF7A00]"value="-" />
+                    <input type="text" id="number" value="1" class=" w-8 text-center" disabled="disabled">
+                    <input type="button" onclick="incrementValue()" class="px-1 font-extrabold text-xl text-[#FF7A00]"value="+" />
+
+                </form>
+            </div>
+            <div class="col-span-1 row-span-1 flex items-center justify-center">
                 <p>118€</p>
             </div>
-
         </div>
-        <div class='row-span-4 col-span-2  outline outline-neutral-400'>
+        <div class='row-span-4 col-span-2 flex justify-evenly flex-col items-center outline outline-neutral-400'>
+            <div class="flex justify-between w-full px-8">
+                <p> Sous-total</p>
 
+                <p>118€</p>
+            </div>
+            <hr class=" bg-black w-4/5 ">
+            <div class="flex justify-between w-full px-8">
+                <p>Livraison</p>
+                <p>5€</p>
+            </div>
+            <hr class=" bg-black w-4/5 ">
+
+            <div class="flex font-extrabold justify-between w-full px-8">
+                <p>total TTC</p>
+                <p>128€</p>
+            </div>
+            <hr class=" bg-black w-4/5 ">
+
+            <button class="rounded font-bold w-3/4  p-2 bg-[#D2F306]"> Poursuivre la commande</button>
         </div>
-
-
-
     </div>
 @endsection
--- resources/views/hero.blade.php
+++ resources/views/hero.blade.php
@@ -1,7 +1,7 @@
 
  <div class=" relative ">
-    <img src="{{URL::asset('Image/HeroHomepage.png')}}" alt="" class="relative">
-    <div class="  max-w-[1400px] mx-auto absolute left-1/4 top-10 object-fill ">
+    <img src="{{URL::asset('Image/HeroHomepage.png')}}" alt="" class="relative w-full">
+    <div class="  max-w-[1400px] mx-auto absolute left-1/4 top-10  ">
         <img src="{{ URL::asset('Image/LogoRunIt.svg') }}" alt="logo" class=" absolute top-28 w-3/4 opacity-25">
         <div class="  text-white z-50 mt-4 sm:mt-20 md:mt-60   ">
             <h1 class=" text-5xl ">SO CLOSE TO THE</h1>
