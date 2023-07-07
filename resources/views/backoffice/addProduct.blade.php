f67e22a856aa808b698193ddfe20a07747d95a6f
Quentin <quentin.sirjean@le-campus-numerique.fr>
2023-07-07 17:04:19 +0200

    changed validation

--- resources/views/backoffice/addProduct.blade.php
+++ resources/views/backoffice/addProduct.blade.php
@@ -14,26 +14,26 @@
         <div class="mb-6">
             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom du
                 Produit</label>
-            <input type="text" id="name" name="name" required
+            <input type="text" id="name" name="name"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
         <div class="mb-6">
             <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prix du
                 Produit en Centimes</label>
-            <input type="number" step="0.01" id="price" name="price" required
+            <input type="number" step="0.01" id="price" name="price"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
         <div class="mb-6">
             <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">URL de
                 l'image d'illustration produit</label>
-            <input type="text" id="image" name="image" required
+            <input type="text" id="image" name="image"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
 
         <div class="mb-6">
             <label for="oneliner" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phrase
                 d'accroche du produit</label>
-            <input type="text" id="oneliner" name="oneliner" required
+            <input type="text" id="oneliner" name="oneliner"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
         <div class="mb-6">
@@ -41,14 +41,14 @@
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description
                 détaillé du produit
             </label>
-            <textarea id="description" name="description" required
+            <textarea id="description" name="description"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </textarea>
         </div>
         <div class="mb-6">
             <label for="stock" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock
                 disponible du produit</label>
-            <input type="number" id="stock" name="stock" required
+            <input type="number" id="stock" name="stock"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
         <div class="flex justify-center">
