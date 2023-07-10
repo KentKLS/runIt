969b9770ea4c7521cc0c53fba45255fd263d4ec7
Quentin <quentin.sirjean@le-campus-numerique.fr>
2023-07-10 16:37:51 +0200

    Maj des migrations

--- database/migrations/2023_07_10_074011_create_categories_table.php
+++ database/migrations/2023_07_10_074011_create_categories_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',150);
             $table->timestamps();
-
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-
+        Schema::dropIfExists('categories');
     }
 };
