f1501642a742d7626be3a95b8f832b925b341891
Quentin <quentin.sirjean@le-campus-numerique.fr>
2023-07-13 16:45:11 +0200

    changement css js

--- public/script.js
+++ public/script.js
@@ -1,14 +1,27 @@
-function incrementValue() {
-    var value = parseInt(document.getElementById('number').value, 10);
+
+
+function incrementValue(stock,id) {
+    var value = parseInt(document.getElementById(id).value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
-    document.getElementById('number').value = value;
+    value = value > stock ? stock : value;
+    document.getElementById(id).value = value;
 }
 
-function decrementValue() {
-    var value = parseInt(document.getElementById('number').value, 10);
+function decrementValue(id) {
+    var value = parseInt(document.getElementById(id).value, 10);
     value--;
     value = value <= 1 ? 1 : value;
 
-    document.getElementById('number').value = value;
+    document.getElementById(id).value = value;
 }
+
+function submitForm(formId) {
+    document.getElementById(formId).submit();
+  }
+
+  function formatPrice(price){
+    formatedPrice = price/100;
+    return formatedPrice;
+  }
+
